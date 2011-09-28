d8450bdc6527491e0548bdff9f8f25f1215ae033
Nikita Romashkin <romashkin.nikita@gmail.com>
2011-09-28 04:23:48 +0400

    TestCase for bug which needs to be fixed

--- fca/algorithms/dg_basistest.py
+++ fca/algorithms/dg_basistest.py
@@ -75,6 +75,29 @@ class RelativeBasisTest(unittest.TestCase):
         self.assertEqual(len(set(relative_basis) & set(self.basis)), 0)
         self.assertEqual(len(relative_basis), 2)
         self.assertEqual(len(self.basis), 1)
+
+class BasisTest2(unittest.TestCase):
+    def setUp(self):
+        ct = [[True, True, True, True],\
+              [True, False, True, False],\
+              [False, True, True, False],\
+              [False, True, True, True]]
+        objs = ['1', '2', '3', '4']
+        attrs = ['a', 'b', 'c', 'd']
+        self.cxt = fca.Context(ct, objs, attrs)
+    
+    def test_relative_basis(self):
+        simple_basis = compute_dg_basis(self.cxt)
+        
+        message = "Implication [{0}] occurs more than once"
+        for imp1 in simple_basis:
+            counter = 0
+            for imp2 in simple_basis:
+                if imp1 == imp2:
+                    counter += 1
+            self.assertEqual(counter, 1, message.format(imp1))
+
+        
         
 
 
